0ae44292bb2c36f9e695308183ac91f28cab4f4d
Dogan-87 <doqanonay@gmail.com>
2021-10-21 17:06:19 +0000

    Temporary changes

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -4,17 +4,17 @@ from translator import english_to_french, french_to_english
 
 class Teste2f(unittest.TestCase):
     def test1(self):
-        self.assertEqual(english_to_french(''), None) # test when 2 is given as input the output is 4.
-    
+        self.assertEqual(english_to_french(''), None) 
     def test2(self):
-        self.assertEqual(english_to_french('Hello'), 'Bonjour')  # test when 3.0 is given as input the output is 9.0.
-        
+        self.assertEqual(english_to_french('Hello'), 'Bonjour') 
+        self.assertNotEqual(english_to_french('Hello'), 'Hola')  
+
 class Testf2e(unittest.TestCase):
     def test1(self):
-        self.assertEqual(french_to_english(''), None) # test when 2 is given as input the output is 4.
-        
+        self.assertEqual(french_to_english(''), None) 
     def test2(self):
-        self.assertEqual(french_to_english('Bonjour'), 'Hello') # test when -3.1 is given as input the output is -6.2.
+        self.assertEqual(french_to_english('Bonjour'), 'Hello') 
+        self.assertNotEqual(english_to_french('Bonjour'), 'Hola')  
 
 if __name__ == "__main__":
     unittest.main()
